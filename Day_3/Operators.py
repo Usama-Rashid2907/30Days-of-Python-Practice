20df4ba0d518a70497d748cc1e9d2d91108fd034
Usama-Rashid2907 <rashidusama334@gmail.com>
2026-06-09 15:54:45 +0500

    Adding a comment here to test github push

---
+++ Day_3/Operators.py
@@ -0,0 +1,23 @@
+# Git is a memory card for code
+# Version control - save your progress so you dont lose it as you go
+# Enter commands to save your progress
+# Save them so if they get deleted, you can go back to last saved
+# Initialize your folder or project with git
+# Have git downloaded and installed
+# Type git init (your memory card is in your system)
+# Git add is how we save our progress
+# Save all changes since last time we saved
+# Or you can save something specific
+# Add a file after git add if you want to save something specific
+# Git add . would save everything.
+# Most people push changes on everything
+# Git add. is the most used
+# All files are in saving
+# Git commit actually saves those changes to memory
+# Git commit - m 'This is the commit message'
+# Git commit - m 'Add HTML and CSS file'
+# Git add.
+# I have a feeling git add. saves all changes since last change to staging
+# Git commit -m committs those changes to memory along with a message
+# Add the files you wanna save, and then committ
+# Git log gives us our saved files. Its a log of all saved changes
