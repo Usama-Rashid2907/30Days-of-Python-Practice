3a9cee11670c81e25a60248d3190224d62d0d796
Usama-Rashid2907 <rashidusama334@gmail.com>
2026-06-12 18:08:51 +0500

    And, or and NOT operators

--- Day_3/Operators.py
+++ Day_3/Operators.py
@@ -317,3 +317,18 @@ print ('coding' in 'I am coding') # This should be true as well
 print ('coding' in 'I am coding in python') # This should be true as well
 print('a in an:', 'a' in 'an')      # True
 print('4 is 2 ** 2:', 4 is 2 ** 2)   # True, I can use expressions and calculations as well
+
+
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False:', True or False)
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
