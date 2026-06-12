3d51f14bf78e0d9b50623a3be0284368e6e204c3
Usama-Rashid2907 <rashidusama334@gmail.com>
2026-06-12 17:22:12 +0500

    Using a and b as variables for operators testing

--- Day_3/Operators.py
+++ Day_3/Operators.py
@@ -214,3 +214,27 @@ BUT
 If i write a calculation, with an operator, i can then print the number out as well
 Wonder if this works with varibles as well. I might be able to calculate them out too
 '''
+
+# Declaring the variable at the top first
+
+a = 3 # a is a variable name and 3 is an integer data type
+b = 2 # b is a variable name and 3 is an integer data type
+
+# Arithmetic operations and assigning the result to a variable
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+# I should have used sum instead of total but sum is a built-in function - try to avoid overriding built-in functions
+print('total = ', total) # if you do not label your print with some string, you never know where the result is coming from
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
