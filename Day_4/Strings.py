79e99fd1a860aaf0bd4541a74292920237be8109
Usama-Rashid2907 <rashidusama334@gmail.com>
2026-06-18 16:28:33 +0500

    Printing basic letters and sentences

---
+++ Day_4/Strings.py
@@ -0,0 +1,10 @@
+# Length of a string using len() method
+letter = 'p'
+print(letter)
+print(len(letter))
+greeting = 'Hello, World!'
+print(greeting)
+print(len(greeting))
+sentence = 'Hope youre loving this python journey!'
+print(sentence)
+print(len(sentence))
