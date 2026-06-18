bc45aae91dee5c8d2c4b4e25165b4d8e74b5630b
Usama-Rashid2907 <rashidusama334@gmail.com>
2026-06-18 16:31:22 +0500

    Multiline strings

--- Day_4/Strings.py
+++ Day_4/Strings.py
@@ -8,3 +8,21 @@ print(len(greeting))
 sentence = 'Hope youre loving this python journey!'
 print(sentence)
 print(len(sentence))
+
+# Strings can also be multi quotes
+Multi_line_string = '''This is a multi-line string.
+It can span multiple lines. I am a teacher
+who enjoys teaching python programming.
+I didnt find anything as emppowering as educating people.
+This is why the 30 days of python exists'''
+print(Multi_line_string)
+print(len(Multi_line_string))
+
+# Another way of doing the same thing
+multi_line_string2 = """This is a multi-line string.
+It can span multiple lines. I am a teacher
+who enjoys teaching python programming. 
+I didnt find anything as emppowering as educating people.
+This is why the 30 days of python exists"""
+print(multi_line_string2)
+print(len(multi_line_string2))
