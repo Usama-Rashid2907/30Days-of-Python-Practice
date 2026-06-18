bfc670357472a0ed9bc4901bbd88401027ba3167
Usama-Rashid2907 <rashidusama334@gmail.com>
2026-06-18 16:34:07 +0500

    Length of concatenated strings

--- Day_4/Strings.py
+++ Day_4/Strings.py
@@ -35,3 +35,8 @@ last_name = 'Rashid'
 space = ' '
 full_name = first_name + space + last_name
 print('Full name = ' + full_name)
+
+print(len(first_name))  
+print(len(last_name))   
+print(len(first_name) > len(last_name)) 
+print(len(full_name)) 
